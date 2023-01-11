70b20a34779f80487610418523bb89780f4f3316
javier mosquera <andrubjj@gmail.com>
2023-01-11 16:04:25 -0500

    elimine las terminadas en css

---
+++ mi-segunda-aplicacion/src/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return(
+        <header>
+            <div>
+                <img src="logo.src" alt="logo" />
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/servicios">Servicios</a></li>
+                        <li><a href="/contactos">Contàctos</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header
